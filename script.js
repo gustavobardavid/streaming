445bb8884eeaace27757650b35caeb60b7a3150d
gustavo <gustavo.cesariosilva15@gmail.com>
2023-07-23 18:06:43 -0300

    adicionando header e imagens

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load" , () => {
+    let botao = document.querySelector("#buscar");
+    botao.addEventListener('click' , () => {
+        let input = document.querySelector('#input-buscar');
+    })
+})
